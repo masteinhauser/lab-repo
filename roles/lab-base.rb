fb14fc75546f962a91d8c642d41798d2ce195058
mattray <matthewhray@gmail.com>
2012-10-05 09:14:59 -0500

    add ubuntu back for inaccessible systems

--- roles/lab-base.rb
+++ roles/lab-base.rb
@@ -14,7 +14,7 @@ default_attributes(
   "authorization" => {
     "sudo" => {
       "groups" => ["admin", "wheel", "sysadmin"],
-      "users" => ["mray"],
+      "users" => ["mray","ubuntu"],
       "passwordless" => true,
       "include_sudoers_d" => true
     }
